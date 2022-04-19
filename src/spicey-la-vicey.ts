4468628c7ef4ae6092bf45a14664d64d024c6d91
Jochem <info@jochemkeller.nl>
2022-04-19 22:10:34 +0200

    feat: add spicey-la-vicey
    Signed-off-by: Jochem <info@jochemkeller.nl>

---
+++ src/spicey-la-vicey.ts
@@ -0,0 +1,65 @@
+import fetch from 'cross-fetch';
+
+import {
+    getTitle,
+    getLink,
+    getTimestamp,
+    getDescription,
+} from '@utils/spicey-la-vicey';
+
+export type Mix = {
+    description: string;
+    link: string;
+    timestamp: number;
+    title: string;
+}
+
+export type Mixes = Mix[];
+
+export const formatMixes = (response: string): Mixes => {
+    const cleaned = response.replace(/^\s+|\s+$/g, '').replace(/(\r\n|\n|\r)/gm, '');
+
+    const cards = cleaned.match(/<article.*?class=".*?sc-c-playable-list-card.*?>(.*?)<\/article>/g) || [];
+
+    return cards.map(card => ({
+        description: getDescription(card),
+        link: getLink(card),
+        timestamp: getTimestamp(card),
+        title: getTitle(card),
+    }));
+};
+
+export const getMixes = async (): Promise<Mixes> => {
+    const request = await fetch('https://www.bbc.co.uk/sounds/brand/b09c12lj');
+    const response = await request.text();
+
+    return formatMixes(response);
+};
+
+export const getNewestMix = async (): Promise<Mix> => {
+    const mixes = await getMixes();
+
+    const [newestMix] = mixes.sort((a, b): number => b.timestamp - a.timestamp);
+
+    return newestMix;
+};
+
+export const getWebhook = async (mix: Mix): Promise<void> => {
+    await fetch('https://discord.com/api/webhooks/964154301582835712/WzolPxY6Ve_vaz2rGwAM5zkINsLpQEwq5tFHiwNLvmc5BTUcIJo8TttzZ3Fwpbmh4g4P', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            content: `🌶🌶🌶\n**${mix.title}**\n<${mix.link}>\n🌶🌶🌶`,
+            username: 'Spicey LaVicey',
+            embeds: [
+                {
+                    image: {
+                        url: 'https://emojis.slackmojis.com/emojis/images/1643509700/43992/hyper-drum-time.gif?1643509700',
+                    },
+                },
+            ],
+        }),
+    });
+};
