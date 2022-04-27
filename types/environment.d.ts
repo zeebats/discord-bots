52d6b9d9a226243c3595774048cc0d1f579fe40e
Daan Vos de Wael <daan@vosdewael.com>
2022-04-27 13:13:35 +0200

    feat: add `environment` declaration file

---
+++ types/environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        SUPABASE_API_KEY: string;
+        SUPABASE_URL: string;
+        WEBHOOK_MOVIES: string;
+        WEBHOOK_SHOWS: string;
+        WEBHOOK_SPICEY_LA_VICEY: string;
+    }
+}
