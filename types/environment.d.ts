56a8888b7dbb8eddeba90969cd229d56caba202c
Jochem <info@jochemkeller.nl>
2022-06-09 21:27:59 +0200

    fix: typecheck errors

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -3,7 +3,7 @@ namespace NodeJS {
         SUPABASE_API_KEY: string;
         SUPABASE_URL: string;
         WEBHOOK_MOVIES: string;
-        WEBHOOK_SHOWS: string;
+        WEBHOOK_SHOWS: string | undefined;
         WEBHOOK_SPICEY_LA_VICEY: string;
     }
 }
