23adb0f831675aeab7bb144d1a0d29ee0d10900a
Jochem <info@jochemkeller.nl>
2022-06-16 11:55:44 +0200

    feat: add case-police case checking

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,5 @@
 export default {
+    '*.*': filenames => [`case-police  --fix ${filenames.join(' ')}`],
     '*.{json,mjs,ts}?(x)': filenames => [
         `eslint ${filenames.join(' ')}`,
         `vitest ${filenames.join(' ')} --passWithNoTests`,
