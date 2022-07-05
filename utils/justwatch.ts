b3460ad727fee9dbf1396d372c16fc6559873dbc
Daan Vos de Wael <daan@vosdewael.com>
2022-07-05 18:29:59 +0200

    fix: indent all code with tabs

--- utils/justwatch.ts
+++ utils/justwatch.ts
@@ -7,51 +7,51 @@ const { NETLIFY_DEV } = process.env;
 const parse = NETLIFY_DEV ? parseLocal : parseProduction;
 
 export const getTitle = (item: HTMLElement | null): string => {
-    const element = item?.querySelector('.picture-comp__img')?.getAttribute('alt') || item?.querySelector('.title-poster--no-poster')?.innerText;
+	const element = item?.querySelector('.picture-comp__img')?.getAttribute('alt') || item?.querySelector('.title-poster--no-poster')?.innerText;
 
-    return element?.replace(/\s*-\s*season \d*/i, '') || '';
+	return element?.replace(/\s*-\s*season \d*/i, '') || '';
 };
 
 export const getThumbnail = (item: HTMLElement | null): string => {
-    const source = item?.querySelector('source[type=image/jpeg]');
+	const source = item?.querySelector('source[type=image/jpeg]');
 
-    const attribute = source?.getAttribute('srcset') || source?.getAttribute('data-srcset') || '';
+	const attribute = source?.getAttribute('srcset') || source?.getAttribute('data-srcset') || '';
 
-    return attribute?.replace(/.*?, (.*?)/, '$1');
+	return attribute?.replace(/.*?, (.*?)/, '$1');
 };
 
 export const getLink = (element: HTMLElement | null): string => {
-    const link = element?.querySelector('a')?.getAttribute('href') || '';
+	const link = element?.querySelector('a')?.getAttribute('href') || '';
 
-    return `https://www.justwatch.com${link}`;
+	return `https://www.justwatch.com${link}`;
 };
 
 export const getToday = (string: string): HTMLElement | null => {
-    const body = parse(string);
+	const body = parse(string);
 
-    return body.querySelector('.timeline__timeframe:first-child');
+	return body.querySelector('.timeline__timeframe:first-child');
 };
 
 export const getProviders = (element: HTMLElement | null): HTMLElement[] => {
-    const providers = element?.querySelectorAll('.timeline__provider-block');
+	const providers = element?.querySelectorAll('.timeline__provider-block');
 
-    return providers || [];
+	return providers || [];
 };
 
 export const getProvider = (element: HTMLElement | null): string => {
-    const provider = element?.querySelector('.provider-timeline__logo');
+	const provider = element?.querySelector('.provider-timeline__logo');
 
-    return provider?.getAttribute('alt') || '';
+	return provider?.getAttribute('alt') || '';
 };
 
 export const getShortcode = (element: HTMLElement | null): string => {
-    const classes = element?.getAttribute('class');
+	const classes = element?.getAttribute('class');
 
-    return classes?.replace(/.*?--\d{4}-\d{2}-\d{2}--(.*?)/, '$1') || '';
+	return classes?.replace(/.*?--\d{4}-\d{2}-\d{2}--(.*?)/, '$1') || '';
 };
 
 export const getColor = (element: HTMLElement | null): number => {
-    const provider = getShortcode(element);
+	const provider = getShortcode(element);
 
-    return selectedProviders[provider]?.color || 15_724_527;
+	return selectedProviders[provider]?.color || 15_724_527;
 };
--- utils/spicey-la-vicey.ts
+++ utils/spicey-la-vicey.ts
@@ -2,57 +2,57 @@ import { getUnixTime, parse, startOfDay } from 'date-fns';
 import { decode } from 'html-entities';
 
 export const getTitle = (string: string): string => {
-    const match = (/<.*?class=".*?sc-c-metadata__primary.*?>(.*?)<\/.*>/g).exec(string);
+	const match = (/<.*?class=".*?sc-c-metadata__primary.*?>(.*?)<\/.*>/g).exec(string);
 
-    if (match === null) {
-        return '';
-    }
+	if (match === null) {
+		return '';
+	}
 
-    const [
-        , value,
-    ] = match;
+	const [
+		, value,
+	] = match;
 
-    return decode(value);
+	return decode(value);
 };
 
 export const getDescription = (string: string): string => {
-    const match = (/<.*?class=".*?sc-c-metadata__synopsis.*?>(.*?)<\/.*>/g).exec(string);
+	const match = (/<.*?class=".*?sc-c-metadata__synopsis.*?>(.*?)<\/.*>/g).exec(string);
 
-    if (match === null) {
-        return '';
-    }
+	if (match === null) {
+		return '';
+	}
 
-    const [
-        , value,
-    ] = match;
+	const [
+		, value,
+	] = match;
 
-    return decode(value);
+	return decode(value);
 };
 
 export const getLink = (string: string): string => {
-    const match = (/<.*?class=".*?sc-c-playable-list-card__link.*? href="(.*?)"/g).exec(string);
+	const match = (/<.*?class=".*?sc-c-playable-list-card__link.*? href="(.*?)"/g).exec(string);
 
-    if (match === null) {
-        return '';
-    }
+	if (match === null) {
+		return '';
+	}
 
-    const [
-        , value,
-    ] = match;
+	const [
+		, value,
+	] = match;
 
-    return decode(`https://www.bbc.co.uk${value}`);
+	return decode(`https://www.bbc.co.uk${value}`);
 };
 
 export const getTimestamp = (string: string): number => {
-    const match = (/<.*?class=".*?sc-c-metadata__release-date.*?>(.*?)<\/.*>/g).exec(string);
+	const match = (/<.*?class=".*?sc-c-metadata__release-date.*?>(.*?)<\/.*>/g).exec(string);
 
-    if (match === null) {
-        return 0;
-    }
+	if (match === null) {
+		return 0;
+	}
 
-    const [
-        , value,
-    ] = match;
+	const [
+		, value,
+	] = match;
 
-    return getUnixTime(startOfDay(parse(value, 'dd LLL yyyy', Date.now())));
+	return getUnixTime(startOfDay(parse(value, 'dd LLL yyyy', Date.now())));
 };
