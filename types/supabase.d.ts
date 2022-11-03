43b1b0f5847330e47892ab86e88d8d00cc094078
Jochem <info@jochemkeller.nl>
2022-11-03 22:20:55 +0100

    feat: implement supabase cli

---
+++ types/supabase.d.ts
@@ -0,0 +1,44 @@
+export type JSON =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: JSON }
+  | JSON[]
+
+export interface Database {
+  public: {
+    Tables: {
+      "spicey-la-vicey": {
+        Row: {
+          id: number
+          timestamp: number | null
+          title: string | null
+          update: boolean | null
+        }
+        Insert: {
+          id?: number
+          timestamp?: number | null
+          title?: string | null
+          update?: boolean | null
+        }
+        Update: {
+          id?: number
+          timestamp?: number | null
+          title?: string | null
+          update?: boolean | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
+
