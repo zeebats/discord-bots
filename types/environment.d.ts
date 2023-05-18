1fd833e3241aec2374e3501d9d2487260fa20e12
Daan Vos de Wael <daan@vosdewael.com>
2023-05-18 09:26:28 +0200

    revert: remove all `insta-meme` source code

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,7 +2,6 @@ namespace NodeJS {
     interface ProcessEnvironment {
         SUPABASE_API_KEY: string;
         SUPABASE_URL: string;
-        WEBHOOK_INSTA_MEME: string;
         WEBHOOK_MOVIES: string;
         WEBHOOK_SHOWS: string | undefined;
         WEBHOOK_SPICEY_LA_VICEY: string;
